5b04cfee9d4d796b498c96912c5086af93a547f7
KousukeFujii_Win7_DeskTopPC <konkon.317@gmail.com>
2017-08-27 20:03:24 +0900

    追加  テクスチャクラス
    branch : Shader/dev_1

---
+++ Shader1/Source/Direct3D/texture.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "Direct3d.h"
+
+class Texture
+{
+	friend class Direct3D;
+private:
+	IDirect3DTexture9* pTexture;
+
+public:
+	
+	//コンストラクタ デストラクタ
+	Texture();
+	~Texture();
+
+	//UVの分割数
+	unsigned int divU;
+	unsigned int divV;
+	//UVの番号
+	unsigned int numU;
+	unsigned int numV;
+
+	void SetDivide(unsigned int DivU, unsigned int DivV);
+	void SetUVNum(unsigned int NumU, unsigned int NumV);
+
+	bool Load(TCHAR* FileName);
+
+
+};
