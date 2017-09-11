0404ed8ee518147b3388720d1dc88c25cbed958c
KousukeFujii_Win7_DeskTopPC <konkon.317@gmail.com>
2017-09-12 02:43:38 +0900

    追加　BasicShaderのエフェクトインスタンス作製　キーで適用するシェーダの変更
    branch : DXBase/dev_sub1

--- DirectX9_Base/Source/Scene/Scene/MainScene.h
+++ DirectX9_Base/Source/Scene/Scene/MainScene.h
@@ -19,6 +19,7 @@
 
 
 #include "../../Direct3D/Effect/EffectLambert.h"
+#include "../../Direct3D/Effect/EffectBasic.h"
 
 class MainScene :public Scene
 {
@@ -39,6 +40,7 @@ private:
 
 	Model model;
 	EffectLambert effectLambert;
+	EffectBasic effectBasic;
 
 	GameObject3D* pGameObject[2];
 
