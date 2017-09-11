dd04fcb3f96c8e83ed7112b9cf56b526ce16d910
KousukeFujii_win8.1_NotePC <konkon.317@gmail.com>
2017-09-11 16:37:04 +0900

    追加　エフェクトクラス　特定の関数が呼ばれるときに自動で呼ばれるパラメータを更新するための仮想関数
    branch : DX9Base/dev_sub1

--- DirectX9_Base/Source/Direct3D/Effect.h
+++ DirectX9_Base/Source/Direct3D/Effect.h
@@ -31,34 +31,53 @@ public :
 		return pEffect->SetTexture(hParamater, texture);
 	}
 
+	//テクニックのセット
 	HRESULT SetTechnique(D3DXHANDLE hParamater)
 	{
 		if (pEffect == nullptr)return E_FAIL;
+
+		this->SetupParameter_OnSetTechnique(hParamater);
+
 		return pEffect->SetTechnique(hParamater);
 	}
 
+
+	//エフェクト使用の開始
 	HRESULT Begine(UINT*pPasses, DWORD Flag)
 	{
 		if (pEffect == nullptr)return E_FAIL;
+
+		this->SetupParameter_OnBegin();
 		return pEffect->Begin(pPasses,Flag);
 	}
 
+	//パスの開始
 	HRESULT BeginePass(UINT pass) 
 	{
 		if (pEffect == nullptr)return E_FAIL;
+
+		this->SetupParameter_OnBeginPass(pass);
 		return pEffect->BeginPass(pass);
 	}
 
+	//パスの終了
 	HRESULT EndPass()
 	{
 		if (pEffect == nullptr)return E_FAIL;
 		return pEffect->EndPass();
 	}
 
+	//エフェクト使用の終了
 	HRESULT End() 
 	{
 		if (pEffect == nullptr)return E_FAIL;
 		return pEffect->End();
-	}
+	}	
+
+protected:
+	//
+	virtual void SetupParameter_OnSetTechnique(D3DXHANDLE hParamater) {};// = 0;
+	virtual void SetupParameter_OnBegin() {};// = 0;
+	virtual void SetupParameter_OnBeginPass(UINT pass) {};// = 0;
 		
 };
